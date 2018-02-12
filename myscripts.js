fb0e35ffb4345483059c1e895cb36eb91bbfff99
Jefferson Mendes <mendesJeffersonn@gmail.com>
2018-02-12 15:35:04 -0200

    Initial commit
    Basic index structure and cv data

---
+++ myscripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('[data-toggle="popover-1"]').popover({title: "<h5><code>Atividades desenvolvidas</code></h5>",content: "<p>-Organizador do Desafio Jovem Engenheiro</p><p>-Responsável pelo Torneio ROBOCODE</p><p>-Desenvolvimento de atividade de apoio à pesquisa e condução de ações do laboratório</p><p>-Apoio ao professor coordenador em atividade de ensino, pesquisas e extensão</p><p>-Auxilio na coordenação</p><p>-Desenvolvimento e aplicação de oficinas de Scratch para crianças</p><p>-Criação e manutenção de páginas web(Wordpress, HTML, PHP, JS, CSS, FTP)</p>",html: true, placement: "right"});
+});
+
+$(document).ready(function(){
+    $('[data-toggle="popover-2"]').popover({title: "<h5><code>Atividades desenvolvidas</code></h5>",content: "<p>-Desenvolvimento de conteúdos para oficina de robótica e auxílio em pesquisas de IC's</p>", html: true, placement: "right"});
+});
+
+$(document).ready(function(){
+    $('[data-toggle="popover-3"]').popover({title: "<h5><code>Atividades desenvolvidas</code></h5>",content: "<p>-Nas experiências profissionais não relacionadas à TI desenvolvi habilidades de gestão, trabalho em equipe, getsão da qualidade e como lidar com clientes. Além disso, foram experiências que me mostraram que o interesse por Tecnologia poderia ser mais que um hobby.</p>", html: true, placement: "right"});
+});
+
+$(document).ready(function(){
+    $('[data-toggle="popover-4"]').popover({title: "<h5><code>Atividades desenvolvidas</code></h5>",content: "<p>-Durante a monitora tive a oportunidade de utilizar <code>HTML</code>, <code>JS</code>, <code>PHP</code> e <code>CSS</code> junto com o <code>WORDPRESS</code> para criar uma ferramenta de gameficação da respectiva disciplina. Está ferramenta irá auxiliar o professor ministrar as aulas das próximas turmas.</p>", html: true, placement: "right"});
+});
