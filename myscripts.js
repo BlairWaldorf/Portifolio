1390a5b3ac1bbefff776043abb187f852e38e419
Jefferson Mendes <mendesJeffersonn@gmail.com>
2018-02-13 21:18:02 -0200

    commit

--- myscripts.js
+++ myscripts.js
@@ -1,18 +1,3 @@
 $(document).ready(function(){
-    $('[data-toggle="popover-1"]').popover({title: "<h5><code>Atividades desenvolvidas</code></h5>",content: "<p>-Organizador do Desafio Jovem Engenheiro</p><p>-Responsável pelo Torneio ROBOCODE</p><p>-Desenvolvimento de atividade de apoio à pesquisa e condução de ações do laboratório</p><p>-Apoio ao professor coordenador em atividade de ensino, pesquisas e extensão</p><p>-Auxilio na coordenação</p><p>-Desenvolvimento e aplicação de oficinas de Scratch para crianças</p><p>-Criação e manutenção de páginas web(Wordpress, HTML, PHP, JS, CSS, FTP)</p>",html: true, placement: "right"});
+    $('[data-toggle="popover-3"]').popover({title: "<h3><code>Atividades desenvolvidas</code></h3>",content: "<h4>-Nas experiências profissionais não relacionadas à TI desenvolvi habilidades de gestão, trabalho em equipe, gestão da qualidade e como lidar com clientes. Além disso, foram experiências que me mostraram que o interesse por Tecnologia poderia ser mais que um hobby, pois tive a oportunidade de participar de etapas de desenvolvimento de sistemas desde levantamento de requisitos até testes de software.</h4>", html: true, placement: "left"});
 });
-
-$(document).ready(function(){
-    $('[data-toggle="popover-2"]').popover({title: "<h5><code>Atividades desenvolvidas</code></h5>",content: "<p>-Desenvolvimento de conteúdos para oficina de robótica e auxílio em pesquisas de IC's</p>", html: true, placement: "right"});
-});
-
-$(document).ready(function(){
-    $('[data-toggle="popover-3"]').popover({title: "<h5><code>Atividades desenvolvidas</code></h5>",content: "<p>-Nas experiências profissionais não relacionadas à TI desenvolvi habilidades de gestão, trabalho em equipe, gestão da qualidade e como lidar com clientes. Além disso, foram experiências que me mostraram que o interesse por Tecnologia poderia ser mais que um hobby, pois tive a oportunidade de participar de etapas de desenvolvimento de sistemas desde levantamento de requisitos até testes de software.</p>", html: true, placement: "right"});
-});
-
-// $(document).ready(function() {
-//     $( "[.fas || .glyphicon]" ).each(function( index ) {
-//       var color = $(this).parent().css("background-color");
-//       $(this).css("-webkit-text-stroke","2px ")
-//     });
-// });
